67859babce3216be9a1ab8045ab200b7fce59abf
double-z <zackzondlo@gmail.com>
2015-04-16 01:16:52 -0400

    updating demo stuff

---
+++ docs/examples/cookbooks/vagrant/recipes/test_demo.rb
@@ -0,0 +1,35 @@
+require 'chef/provisioning/ssh_driver'
+require 'chef/provisioning/registry'
+
+registry_machine "192.168.33.22" do
+  machine_types ['app_server']
+  retrieve_specs true
+  ssh_user "vagrant"
+  ssh_pass "vagrant"
+  ssh_keys [::File.join(ENV['HOME'], ".vagrant/insecure_private_key")]
+end
+
+machine_registry "lb" do
+  use_registry true
+  registry_options 'machine_type' => 'lb_server', 'memory' => ['235', '256']
+end
+
+machine_registry "app01" do
+  use_registry true
+  registry_options 'machine_type' => 'app_server', 'memory' => ['235', '256']
+end
+
+machine_registry "app02" do
+  use_registry true
+  registry_options 'machine_type' => 'app_server', 'memory' => ['235', '256']
+end
+
+machine_registry "db" do
+  use_registry true
+  registry_options 'machine_type' => 'db_server', 'memory' => ['235', '256']
+end
+
+machine_registry "cache" do
+  use_registry true
+  registry_options 'machine_type' => 'cache_server', 'memory' => ['235', '256']
+end
